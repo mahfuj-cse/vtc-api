330b65bd95d92f8dbf681ee1b4cefd2bf0ed41e8
Abdullah Al Mahfuj <53569184+mahfuj-cse@users.noreply.github.com>
2023-11-18 21:19:39 +0600

    base class  added

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+
+Route::middleware('auth:api')->group(function () {
+    // Add your protected routes here
+});
+
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
